c7e99a0a11c9cbe0e6cb7f622f240aac8247ed1f
SteveCollie <steve.collie@finbourne.com>
2019-07-15 13:54:33 +0100

    DEV-4190 Enabled xml documentation

--- sdk/Lusid.Sdk/Utilities/ApiConfiguration.cs
+++ sdk/Lusid.Sdk/Utilities/ApiConfiguration.cs
@@ -1,13 +1,43 @@
 namespace Lusid.Sdk.Utilities
 {
+    /// <summary>
+    /// Configuration for the ClientCredentialsFlowTokenProvider, usually sourced from a "secrets.json" file
+    /// </summary>
     public class ApiConfiguration
     {
+        /// <summary>
+        /// Url for the token provider
+        /// </summary>
         public string TokenUrl { get; set; }
+
+        /// <summary>
+        /// Username
+        /// </summary>
         public string Username { get; set; }
+
+        /// <summary>
+        /// Password
+        /// </summary>
         public string Password { get; set; }
+
+        /// <summary>
+        /// OAuth2 Client ID
+        /// </summary>
         public string ClientId { get; set; }
+
+        /// <summary>
+        /// OAuth2 Client Secret
+        /// </summary>
         public string ClientSecret { get;  set; }
+
+        /// <summary>
+        /// LUSID Api Url
+        /// </summary>
         public string ApiUrl { get; set; }
+
+        /// <summary>
+        /// Client Application name
+        /// </summary>
         public string ApplicationName { get; set; }
     }
 }
--- sdk/Lusid.Sdk/Utilities/LusidApiFactoryBuilder.cs
+++ sdk/Lusid.Sdk/Utilities/LusidApiFactoryBuilder.cs
@@ -4,8 +4,14 @@ using Microsoft.Extensions.Configuration;
 
 namespace Lusid.Sdk.Utilities
 {
+    /// <summary>
+    /// Builder class to build instances of ILusidApiFactory
+    /// </summary>
     public class LusidApiFactoryBuilder
     {
+        /// <summary>
+        /// Create an ILusidApiFactory using the specified configuration file.  For details on the format of the configuration file see https://support.lusid.com/getting-started-with-apis-sdks
+        /// </summary>
         public static ILusidApiFactory Build(string apiConfiguration)
         {
             if (apiConfiguration == null) throw new ArgumentNullException(nameof(apiConfiguration));
