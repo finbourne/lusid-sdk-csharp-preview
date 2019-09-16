6e2e5ae75fc5844e5ecdde86c24a8e9654416952
Concourse <concourse@finbourne.com>
2019-09-16 14:51:59 +0000

    Update SDK 0.10.705

--- sdk/Lusid.Sdk/Model/PagedResourceListOfPortfolioGroup.cs
+++ sdk/Lusid.Sdk/Model/PagedResourceListOfPortfolioGroup.cs
@@ -23,33 +23,67 @@ using OpenAPIDateConverter = Lusid.Sdk.Client.OpenAPIDateConverter;
 namespace Lusid.Sdk.Model
 {
     /// <summary>
-    /// ResultDataSchema
+    /// PagedResourceListOfPortfolioGroup
     /// </summary>
     [DataContract]
-    public partial class ResultDataSchema :  IEquatable<ResultDataSchema>
+    public partial class PagedResourceListOfPortfolioGroup :  IEquatable<PagedResourceListOfPortfolioGroup>
     {
         /// <summary>
-        /// Initializes a new instance of the <see cref="ResultDataSchema" /> class.
+        /// Initializes a new instance of the <see cref="PagedResourceListOfPortfolioGroup" /> class.
         /// </summary>
-        /// <param name="nodeValueSchema">nodeValueSchema.</param>
-        /// <param name="propertySchema">propertySchema.</param>
-        public ResultDataSchema(Dictionary<string, FieldSchema> nodeValueSchema = default(Dictionary<string, FieldSchema>), Dictionary<string, FieldSchema> propertySchema = default(Dictionary<string, FieldSchema>))
+        [JsonConstructorAttribute]
+        protected PagedResourceListOfPortfolioGroup() { }
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PagedResourceListOfPortfolioGroup" /> class.
+        /// </summary>
+        /// <param name="values">values (required).</param>
+        /// <param name="href">href.</param>
+        /// <param name="links">links.</param>
+        public PagedResourceListOfPortfolioGroup(List<PortfolioGroup> values = default(List<PortfolioGroup>), string href = default(string), List<Link> links = default(List<Link>))
         {
-            this.NodeValueSchema = nodeValueSchema;
-            this.PropertySchema = propertySchema;
+            // to ensure "values" is required (not null)
+            if (values == null)
+            {
+                throw new InvalidDataException("values is a required property for PagedResourceListOfPortfolioGroup and cannot be null");
+            }
+            else
+            {
+                this.Values = values;
+            }
+            
+            this.Href = href;
+            this.Links = links;
         }
         
         /// <summary>
-        /// Gets or Sets NodeValueSchema
+        /// Gets or Sets NextPage
         /// </summary>
-        [DataMember(Name="nodeValueSchema", EmitDefaultValue=false)]
-        public Dictionary<string, FieldSchema> NodeValueSchema { get; set; }
+        [DataMember(Name="nextPage", EmitDefaultValue=false)]
+        public string NextPage { get; private set; }
 
         /// <summary>
-        /// Gets or Sets PropertySchema
+        /// Gets or Sets PreviousPage
         /// </summary>
-        [DataMember(Name="propertySchema", EmitDefaultValue=false)]
-        public Dictionary<string, FieldSchema> PropertySchema { get; set; }
+        [DataMember(Name="previousPage", EmitDefaultValue=false)]
+        public string PreviousPage { get; private set; }
+
+        /// <summary>
+        /// Gets or Sets Values
+        /// </summary>
+        [DataMember(Name="values", EmitDefaultValue=false)]
+        public List<PortfolioGroup> Values { get; set; }
+
+        /// <summary>
+        /// Gets or Sets Href
+        /// </summary>
+        [DataMember(Name="href", EmitDefaultValue=false)]
+        public string Href { get; set; }
+
+        /// <summary>
+        /// Gets or Sets Links
+        /// </summary>
+        [DataMember(Name="links", EmitDefaultValue=false)]
+        public List<Link> Links { get; set; }
 
         /// <summary>
         /// Returns the string presentation of the object
@@ -58,9 +92,12 @@ namespace Lusid.Sdk.Model
         public override string ToString()
         {
             var sb = new StringBuilder();
-            sb.Append("class ResultDataSchema {\n");
-            sb.Append("  NodeValueSchema: ").Append(NodeValueSchema).Append("\n");
-            sb.Append("  PropertySchema: ").Append(PropertySchema).Append("\n");
+            sb.Append("class PagedResourceListOfPortfolioGroup {\n");
+            sb.Append("  NextPage: ").Append(NextPage).Append("\n");
+            sb.Append("  PreviousPage: ").Append(PreviousPage).Append("\n");
+            sb.Append("  Values: ").Append(Values).Append("\n");
+            sb.Append("  Href: ").Append(Href).Append("\n");
+            sb.Append("  Links: ").Append(Links).Append("\n");
             sb.Append("}\n");
             return sb.ToString();
         }
@@ -81,31 +118,46 @@ namespace Lusid.Sdk.Model
         /// <returns>Boolean</returns>
         public override bool Equals(object input)
         {
-            return this.Equals(input as ResultDataSchema);
+            return this.Equals(input as PagedResourceListOfPortfolioGroup);
         }
 
         /// <summary>
-        /// Returns true if ResultDataSchema instances are equal
+        /// Returns true if PagedResourceListOfPortfolioGroup instances are equal
         /// </summary>
-        /// <param name="input">Instance of ResultDataSchema to be compared</param>
+        /// <param name="input">Instance of PagedResourceListOfPortfolioGroup to be compared</param>
         /// <returns>Boolean</returns>
-        public bool Equals(ResultDataSchema input)
+        public bool Equals(PagedResourceListOfPortfolioGroup input)
         {
             if (input == null)
                 return false;
 
             return 
                 (
-                    this.NodeValueSchema == input.NodeValueSchema ||
-                    this.NodeValueSchema != null &&
-                    input.NodeValueSchema != null &&
-                    this.NodeValueSchema.SequenceEqual(input.NodeValueSchema)
+                    this.NextPage == input.NextPage ||
+                    (this.NextPage != null &&
+                    this.NextPage.Equals(input.NextPage))
                 ) && 
                 (
-                    this.PropertySchema == input.PropertySchema ||
-                    this.PropertySchema != null &&
-                    input.PropertySchema != null &&
-                    this.PropertySchema.SequenceEqual(input.PropertySchema)
+                    this.PreviousPage == input.PreviousPage ||
+                    (this.PreviousPage != null &&
+                    this.PreviousPage.Equals(input.PreviousPage))
+                ) && 
+                (
+                    this.Values == input.Values ||
+                    this.Values != null &&
+                    input.Values != null &&
+                    this.Values.SequenceEqual(input.Values)
+                ) && 
+                (
+                    this.Href == input.Href ||
+                    (this.Href != null &&
+                    this.Href.Equals(input.Href))
+                ) && 
+                (
+                    this.Links == input.Links ||
+                    this.Links != null &&
+                    input.Links != null &&
+                    this.Links.SequenceEqual(input.Links)
                 );
         }
 
@@ -118,10 +170,16 @@ namespace Lusid.Sdk.Model
             unchecked // Overflow is fine, just wrap
             {
                 int hashCode = 41;
-                if (this.NodeValueSchema != null)
-                    hashCode = hashCode * 59 + this.NodeValueSchema.GetHashCode();
-                if (this.PropertySchema != null)
-                    hashCode = hashCode * 59 + this.PropertySchema.GetHashCode();
+                if (this.NextPage != null)
+                    hashCode = hashCode * 59 + this.NextPage.GetHashCode();
+                if (this.PreviousPage != null)
+                    hashCode = hashCode * 59 + this.PreviousPage.GetHashCode();
+                if (this.Values != null)
+                    hashCode = hashCode * 59 + this.Values.GetHashCode();
+                if (this.Href != null)
+                    hashCode = hashCode * 59 + this.Href.GetHashCode();
+                if (this.Links != null)
+                    hashCode = hashCode * 59 + this.Links.GetHashCode();
                 return hashCode;
             }
         }
