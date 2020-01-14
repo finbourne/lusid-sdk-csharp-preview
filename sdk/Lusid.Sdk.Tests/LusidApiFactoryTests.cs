f3f48310c7b70547108696614490a243c7c1198a
steco <steve.collie@finbourne.com>
2020-01-14 18:13:53 +0000

    Refactor creation of the LusidApiFactory to be threadsafe (#55)
    Co-authored-by: Rizwan Saeed <rizwansaeed@users.noreply.github.com>

--- sdk/Lusid.Sdk.Tests/LusidApiFactoryTests.cs
+++ sdk/Lusid.Sdk.Tests/LusidApiFactoryTests.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 using Lusid.Sdk.Api;
 using Lusid.Sdk.Client;
 using Lusid.Sdk.Model;
@@ -165,6 +167,44 @@ namespace Lusid.Sdk.Tests
             
             Assert.That(errorResponse, Is.Null);
         }
+
+
+        [TestCase(1, 10)]
+        [TestCase(100, 25, Explicit = true)]
+        public void Multi_Threaded_ApiFactory_Tasks(int quoteCount, int threadCount)
+        {
+            var config = ApiConfigurationBuilder.Build("secrets.json");
+            var provider = new ClientCredentialsFlowTokenProvider(config);
+            
+            var date = new DateTimeOffset(2018, 1, 1, 0, 0, 0, TimeSpan.Zero);
+
+            var request = Enumerable.Range(0, quoteCount).Select(i => new UpsertQuoteRequest(
+                new QuoteId(
+                    new QuoteSeriesId(
+                        provider: "DataScope",
+                        priceSource: "BankA",
+                        instrumentId: "BBG000B9XRY4",
+                        instrumentIdType: QuoteSeriesId.InstrumentIdTypeEnum.Figi,
+                        quoteType: QuoteSeriesId.QuoteTypeEnum.Price,
+                        field: "mid"),
+                    effectiveAt: date.AddDays(i)),
+                metricValue: new MetricValue(
+                    value: 199.23,
+                    unit: "USD"),
+                lineage: "InternalSystem")).ToDictionary(k => k.QuoteId.EffectiveAt.ToString(), v => v);
+
+            var tasks = Enumerable.Range(0, threadCount).Select(x => Task.Run(() =>
+            {
+                var factory = LusidApiFactoryBuilder.Build(config.ApiUrl, provider);
+                var result = factory.Api<IQuotesApi>().UpsertQuotes("mt-scope", request);
+                Assert.That(result.Failed, Is.Empty);
+                
+                Console.WriteLine($"{DateTimeOffset.UtcNow} {Thread.CurrentThread.ManagedThreadId} {result.Values.Count}");
+            }));
+
+            Task.WaitAll(tasks.ToArray());
+        }
+
 
     }
 }
--- sdk/Lusid.Sdk/Utilities/LusidApiFactoryBuilder.cs
+++ sdk/Lusid.Sdk/Utilities/LusidApiFactoryBuilder.cs
@@ -1,14 +1,16 @@
-using System;
-using System.IO;
-using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+using System.Threading;
 
 namespace Lusid.Sdk.Utilities
 {
     /// <summary>
     /// Builder class to build instances of ILusidApiFactory
     /// </summary>
-    public class LusidApiFactoryBuilder
+    public static class LusidApiFactoryBuilder
     {
+        private static readonly Dictionary<int, ILusidApiFactory> ThreadFactories = new Dictionary<int, ILusidApiFactory>();
+        private static readonly object Lock = new object();
+
         /// <summary>
         /// Create an ILusidApiFactory using the specified configuration file.  For details on the format of the configuration file see https://support.lusid.com/getting-started-with-apis-sdks
         /// </summary>
@@ -16,6 +18,33 @@ namespace Lusid.Sdk.Utilities
         {
             var apiConfig = ApiConfigurationBuilder.Build(apiSecretsFilename);
             return new LusidApiFactory(apiConfig);
+        }
+
+
+        /// <summary>
+        /// Create an ILusidApiFactory using the specified Url and Token Provider
+        /// </summary>
+        public static ILusidApiFactory Build(string url, ITokenProvider tokenProvider)
+        {
+            lock (Lock)
+            {
+                var threadId = Thread.CurrentThread.ManagedThreadId;
+
+                if (!ThreadFactories.TryGetValue(threadId, out var factory))
+                {
+                    // TokenProviderConfiguration.ApiClient is the client used by LusidApiFactory and is 
+                    // not threadsafe, so there needs to be a separate instance for each instance of LusidApiFactory
+                    var config = new TokenProviderConfiguration(tokenProvider)
+                    {
+                        BasePath = url
+                    };
+
+                    factory = new LusidApiFactory(config);
+                    ThreadFactories[threadId] = factory;
+                }
+
+                return factory;
+            }
         }
     }
 }
