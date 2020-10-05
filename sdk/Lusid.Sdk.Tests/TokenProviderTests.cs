2ade369cdc1da76f05d6d39661c966e7d54f4999
Michael McGarry <mike.mcgarry@finbourne.com>
2020-10-05 09:16:26 +0100

    SE-400 Updated the refresh token logic so that even if both the access token and refresh token are expired a new access token can be generated from Okta

--- sdk/Lusid.Sdk.Tests/TokenProviderTests.cs
+++ sdk/Lusid.Sdk.Tests/TokenProviderTests.cs
@@ -108,6 +108,33 @@ namespace Lusid.Sdk.Tests
             //    all requests must have the same token i.e. reuse a valid token
             Assert.That(requests.GroupBy(r => r.Result).Count(), Is.EqualTo(1), "Requests have different tokens");
         }
+        
+        [Test]
+        public async Task CanGetNewTokenWhenRefreshTokenExpired()
+        {
+            
+            var provider = new ClientCredentialsFlowTokenProvider(ApiConfig.Value);
+            var _ = await provider.GetAuthenticationTokenAsync();
+            var firstTokenDetails = provider.GetLastToken();
+
+            Assert.That(firstTokenDetails.RefreshToken, Is.Not.Null.And.Not.Empty, "refresh_token not returned so unable to verify refresh behaviour.");
+
+            Console.WriteLine($"Token expiring at {firstTokenDetails.ExpiresOn:o}");
+
+            // WHEN we pretend to delay until both...
+            // (1) the original token has expired (for expediency update the expires_on on the token)
+            provider.ExpireToken();
+            // (2) the refresh token has expired (for expediency update the refresh_token to an invalid value that will not be found)
+            provider.GetLastToken().RefreshToken = "InvalidRefreshToken";
+
+            Assert.That(DateTimeOffset.UtcNow, Is.GreaterThan(firstTokenDetails.ExpiresOn));
+            var refreshedToken = await provider.GetAuthenticationTokenAsync();
+
+            // THEN it should be populated, and the ExpiresOn should be in the future
+            Assert.That(refreshedToken, Is.Not.Empty);
+            Assert.That(provider.GetLastToken().ExpiresOn, Is.GreaterThan(DateTimeOffset.UtcNow));
+        }
+
     }
 }
 
