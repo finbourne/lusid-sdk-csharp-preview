24f5534b951b21c7f0816931f01b129dec0ea3b4
Johnny Zhong <johnny.zhong@finbourne.com>
2020-10-15 13:30:51 +0100

    DEV-8909: Valuation examples of instruments

---
+++ sdk/Lusid.Sdk.Tests/Utilities/InstrumentExamples.cs
@@ -0,0 +1,137 @@
+using System;
+using System.Collections.Generic;
+using Lusid.Sdk.Model;
+
+namespace Lusid.Sdk.Tests.Utilities
+{
+    public static class InstrumentExamples
+    {
+        private static readonly DateTimeOffset TestEffectiveAt 
+            = new DateTimeOffset(2020, 2, 23, 0, 0, 0, TimeSpan.Zero);
+        
+        public static LusidInstrument GetExampleInstrument(string instrumentName)
+        {
+            return instrumentName switch
+            {
+                nameof(Bond) => CreateExampleBond(),
+                nameof(FxForward) => CreateExampleFxForward(),
+                nameof(FxOption) => CreateExampleFxOption(),
+                nameof(InterestRateSwap) => CreateExampleSwap(),
+                _ => throw new ArgumentOutOfRangeException($"Please implement case for instrument {instrumentName}")
+            };
+        }
+
+        internal static LusidInstrument CreateExampleFxForward()
+            => new FxForward(
+                domAmount: -1m,
+                fgnAmount: 123m,
+                domCcy: "USD",
+                fgnCcy: "JPY",
+                startDate: new DateTimeOffset(2020, 2, 7, 0, 0, 0, TimeSpan.Zero),
+                maturityDate: new DateTimeOffset(2020, 9, 18, 0, 0, 0, TimeSpan.Zero),
+                instrumentType: LusidInstrument.InstrumentTypeEnum.FxForward
+            );
+
+        internal static LusidInstrument CreateExampleFxOption()
+            => new FxOption(
+                strike: 130,
+                domCcy: "USD",
+                fgnCcy: "JPY",
+                startDate: new DateTimeOffset(2020, 2, 7, 0, 0, 0, TimeSpan.Zero),
+                optionMaturityDate: new DateTimeOffset(2020, 12, 18, 0, 0, 0, TimeSpan.Zero),
+                optionSettlementDate: new DateTimeOffset(2020, 12, 21, 0, 0, 0, TimeSpan.Zero),
+                isCallNotPut: true,
+                isDeliveryNotCash: true,
+                instrumentType: LusidInstrument.InstrumentTypeEnum.FxOption
+            );
+
+        private static FlowConventions CreateExampleFlowConventions()
+            => new FlowConventions(
+                scope: null,
+                code: null,
+                currency: "USD",
+                paymentFrequency: "6M",
+                rollConvention: FlowConventions.RollConventionEnum.MF,
+                dayCountConvention: FlowConventions.DayCountConventionEnum.Act365,
+                holidayCalendars: new List<string>(),
+                settleDays: 2,
+                resetDays: 2
+            );
+
+        internal static LusidInstrument CreateExampleBond()
+            => new Bond(
+                startDate: new DateTimeOffset(2020, 2, 7, 0, 0, 0, TimeSpan.Zero),
+                maturityDate: new DateTimeOffset(2020, 9, 18, 0, 0, 0, TimeSpan.Zero),
+                domCcy: "USD",
+                principal: 100m,
+                couponRate: 0.05m,
+                flowConventions: CreateExampleFlowConventions(),
+                identifiers: new Dictionary<string, string>(),
+                instrumentType: LusidInstrument.InstrumentTypeEnum.Bond
+            );
+
+        internal static LusidInstrument CreateExampleSwap()
+        {
+            // CREATE an Interest Rate Swap (IRS) (that can then be upserted into LUSID)
+            var startDate = TestEffectiveAt;
+            var maturityDate = startDate.AddYears(3);
+
+            // CREATE the flow conventions, index convention
+            var idxConvention = new IndexConvention(
+                code: "GbpLibor6m",
+                publicationDayLag: 0,
+                currency: "USD",
+                paymentTenor: "6M",
+                dayCountConvention: IndexConvention.DayCountConventionEnum.Act365,
+                fixingReference: "BP00"
+            );
+
+            // CREATE the leg definitions
+            var fixedLegDef = new LegDefinition(
+                rateOrSpread: 0.02m, // fixed leg rate (swap rate)
+                stubType: LegDefinition.StubTypeEnum.Front,
+                payReceive: LegDefinition.PayReceiveEnum.Pay,
+                notionalExchangeType: LegDefinition.NotionalExchangeTypeEnum.None,
+                conventions: CreateExampleFlowConventions()
+            );
+
+            var floatLegDef = new LegDefinition(
+                rateOrSpread: 0.05m, // float leg spread over curve rate, often zero
+                stubType: LegDefinition.StubTypeEnum.Front,
+                payReceive: LegDefinition.PayReceiveEnum.Pay,
+                notionalExchangeType: LegDefinition.NotionalExchangeTypeEnum.None,
+                conventions: CreateExampleFlowConventions(),
+                indexConvention: idxConvention
+            );
+
+            // CREATE the fixed leg
+            var fixedLeg = new FixedLeg(
+                notional: 100m,
+                startDate: startDate,
+                maturityDate: maturityDate,
+                legDefinition: fixedLegDef,
+                instrumentType: LusidInstrument.InstrumentTypeEnum.FixedLeg
+            );
+
+            // CREATE the floating leg
+            var floatLeg = new FloatingLeg(
+                notional: 100m,
+                startDate: startDate,
+                maturityDate: maturityDate,
+                legDefinition: floatLegDef,
+                instrumentType: LusidInstrument.InstrumentTypeEnum.FloatingLeg
+            );
+
+            return new InterestRateSwap(
+                startDate: startDate,
+                maturityDate: maturityDate,
+                legs: new List<InstrumentLeg>
+                {
+                    floatLeg,
+                    fixedLeg
+                },
+                instrumentType: LusidInstrument.InstrumentTypeEnum.InterestRateSwap
+            );
+        }
+    }
+}
