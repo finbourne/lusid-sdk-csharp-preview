482f21b5dbafeb31044d0cbd6d94b2f7faafb3b5
Concourse <concourse@finbourne.com>
2020-11-13 17:03:33 +0000

    Update SDK 0.11.2285

---
+++ sdk/Lusid.Sdk.Tests/tutorials/Instruments/InstrumentData.cs
@@ -0,0 +1,69 @@
+using System.Collections.Generic;
+using Lusid.Sdk.Api;
+using Lusid.Sdk.Model;
+using Lusid.Sdk.Utilities;
+using NUnit.Framework;
+
+namespace Lusid.Sdk.Tests.Tutorials.MarketData
+{
+    public class InstrumentData
+    {
+        private IConventionsApi _conventionsApi;
+        [OneTimeSetUp]
+        public void SetUp()
+        {
+            var apiFactory = LusidApiFactoryBuilder.Build("secrets.json");
+            _conventionsApi = apiFactory.Api<IConventionsApi>();
+        }
+
+        [Test]
+        public void UpsertConventions()
+        {
+            string scope = "TestConventionsScope";
+            string flowConventionsCode = "GBP-6m";
+            string indexConventionCode = "Gbp-Libor-6m";
+
+            // CREATE the flow conventions, index convention for swap
+            var flowConventions = new FlowConventions(
+                scope: scope,
+                code: flowConventionsCode,
+                currency: "GBP",
+                paymentFrequency: "6M",
+                rollConvention: "ModifiedFollowing",
+                dayCountConvention: "Actual365",
+                paymentCalendars: new List<string>(),
+                resetCalendars: new List<string>(),
+                settleDays: 2,
+                resetDays: 2
+            );
+
+            var indexConvention = new IndexConvention(
+                scope: scope,
+                code: indexConventionCode,
+                publicationDayLag: 0,
+                currency: "GBP",
+                paymentTenor: "6M",
+                dayCountConvention: "Actual365",
+                fixingReference: "BP00"
+            );
+
+            var flowConventionsResponse =  _conventionsApi.UpsertFlowConventions(new UpsertFlowConventionsRequest(flowConventions));
+            Assert.That(flowConventionsResponse, Is.Not.Null);
+            Assert.That(flowConventionsResponse.Value, Is.Not.Null);
+
+            var indexConventionsResponse = _conventionsApi.UpsertIndexConvention(new UpsertIndexConventionRequest(indexConvention));
+            Assert.That(indexConventionsResponse, Is.Not.Null);
+            Assert.That(indexConventionsResponse.Value, Is.Not.Null);
+
+            var retrievedFlowConventions = _conventionsApi.GetFlowConventions(scope, flowConventionsCode);
+            Assert.That(retrievedFlowConventions, Is.Not.Null);
+            Assert.That(retrievedFlowConventions.Value.Scope, Is.EqualTo(flowConventions.Scope));
+            Assert.That(retrievedFlowConventions.Value.Code, Is.EqualTo(flowConventions.Code));
+
+            var retrievedIndexConvention = _conventionsApi.GetIndexConvention(scope, indexConventionCode);
+            Assert.That(retrievedIndexConvention, Is.Not.Null);
+            Assert.That(retrievedIndexConvention.Value.Scope, Is.EqualTo(indexConvention.Scope));
+            Assert.That(retrievedIndexConvention.Value.Code, Is.EqualTo(indexConvention.Code));
+        }
+    }
+}
