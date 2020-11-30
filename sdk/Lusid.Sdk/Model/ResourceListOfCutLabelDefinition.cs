f1e1a4181fb6f219f9c72258e9927cc49d261f0d
Concourse <concourse@finbourne.com>
2020-11-30 14:03:53 +0000

    Update SDK 0.11.2336

--- sdk/Lusid.Sdk/Model/ResourceListOfCutLabelDefinition.cs
+++ sdk/Lusid.Sdk/Model/ResourceListOfCutLabelDefinition.cs
@@ -23,55 +23,140 @@ using OpenAPIDateConverter = Lusid.Sdk.Client.OpenAPIDateConverter;
 namespace Lusid.Sdk.Model
 {
     /// <summary>
-    /// Defines Operator
+    /// ResourceListOfCutLabelDefinition
     /// </summary>
-    
-    [JsonConverter(typeof(StringEnumConverter))]
-    
-    public enum Operator
+    [DataContract]
+    public partial class ResourceListOfCutLabelDefinition :  IEquatable<ResourceListOfCutLabelDefinition>
     {
         /// <summary>
-        /// Enum Equals for value: Equals
+        /// Initializes a new instance of the <see cref="ResourceListOfCutLabelDefinition" /> class.
         /// </summary>
-        [EnumMember(Value = "Equals")]
-        Equals = 1,
+        [JsonConstructorAttribute]
+        protected ResourceListOfCutLabelDefinition() { }
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ResourceListOfCutLabelDefinition" /> class.
+        /// </summary>
+        /// <param name="values">values (required).</param>
+        /// <param name="href">href.</param>
+        /// <param name="links">links.</param>
+        public ResourceListOfCutLabelDefinition(List<CutLabelDefinition> values = default(List<CutLabelDefinition>), string href = default(string), List<Link> links = default(List<Link>))
+        {
+            // to ensure "values" is required (not null)
+            if (values == null)
+            {
+                throw new InvalidDataException("values is a required property for ResourceListOfCutLabelDefinition and cannot be null");
+            }
+            else
+            {
+                this.Values = values;
+            }
+            
+            this.Href = href;
+            this.Links = links;
+            this.Href = href;
+            this.Links = links;
+        }
+        
+        /// <summary>
+        /// Gets or Sets Values
+        /// </summary>
+        [DataMember(Name="values", EmitDefaultValue=false)]
+        public List<CutLabelDefinition> Values { get; set; }
 
         /// <summary>
-        /// Enum NotEquals for value: NotEquals
+        /// Gets or Sets Href
         /// </summary>
-        [EnumMember(Value = "NotEquals")]
-        NotEquals = 2,
+        [DataMember(Name="href", EmitDefaultValue=true)]
+        public string Href { get; set; }
 
         /// <summary>
-        /// Enum GreaterThan for value: GreaterThan
+        /// Gets or Sets Links
         /// </summary>
-        [EnumMember(Value = "GreaterThan")]
-        GreaterThan = 3,
+        [DataMember(Name="links", EmitDefaultValue=true)]
+        public List<Link> Links { get; set; }
 
         /// <summary>
-        /// Enum GreaterThanOrEqualTo for value: GreaterThanOrEqualTo
+        /// Returns the string presentation of the object
         /// </summary>
-        [EnumMember(Value = "GreaterThanOrEqualTo")]
-        GreaterThanOrEqualTo = 4,
+        /// <returns>String presentation of the object</returns>
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.Append("class ResourceListOfCutLabelDefinition {\n");
+            sb.Append("  Values: ").Append(Values).Append("\n");
+            sb.Append("  Href: ").Append(Href).Append("\n");
+            sb.Append("  Links: ").Append(Links).Append("\n");
+            sb.Append("}\n");
+            return sb.ToString();
+        }
+  
+        /// <summary>
+        /// Returns the JSON string presentation of the object
+        /// </summary>
+        /// <returns>JSON string presentation of the object</returns>
+        public virtual string ToJson()
+        {
+            return JsonConvert.SerializeObject(this, Formatting.Indented);
+        }
 
         /// <summary>
-        /// Enum LessThan for value: LessThan
+        /// Returns true if objects are equal
         /// </summary>
-        [EnumMember(Value = "LessThan")]
-        LessThan = 5,
+        /// <param name="input">Object to be compared</param>
+        /// <returns>Boolean</returns>
+        public override bool Equals(object input)
+        {
+            return this.Equals(input as ResourceListOfCutLabelDefinition);
+        }
 
         /// <summary>
-        /// Enum LessThanOrEqualTo for value: LessThanOrEqualTo
+        /// Returns true if ResourceListOfCutLabelDefinition instances are equal
         /// </summary>
-        [EnumMember(Value = "LessThanOrEqualTo")]
-        LessThanOrEqualTo = 6,
+        /// <param name="input">Instance of ResourceListOfCutLabelDefinition to be compared</param>
+        /// <returns>Boolean</returns>
+        public bool Equals(ResourceListOfCutLabelDefinition input)
+        {
+            if (input == null)
+                return false;
+
+            return 
+                (
+                    this.Values == input.Values ||
+                    this.Values != null &&
+                    input.Values != null &&
+                    this.Values.SequenceEqual(input.Values)
+                ) && 
+                (
+                    this.Href == input.Href ||
+                    (this.Href != null &&
+                    this.Href.Equals(input.Href))
+                ) && 
+                (
+                    this.Links == input.Links ||
+                    this.Links != null &&
+                    input.Links != null &&
+                    this.Links.SequenceEqual(input.Links)
+                );
+        }
 
         /// <summary>
-        /// Enum In for value: In
+        /// Gets the hash code
         /// </summary>
-        [EnumMember(Value = "In")]
-        In = 7
-
+        /// <returns>Hash code</returns>
+        public override int GetHashCode()
+        {
+            unchecked // Overflow is fine, just wrap
+            {
+                int hashCode = 41;
+                if (this.Values != null)
+                    hashCode = hashCode * 59 + this.Values.GetHashCode();
+                if (this.Href != null)
+                    hashCode = hashCode * 59 + this.Href.GetHashCode();
+                if (this.Links != null)
+                    hashCode = hashCode * 59 + this.Links.GetHashCode();
+                return hashCode;
+            }
+        }
     }
 
 }
