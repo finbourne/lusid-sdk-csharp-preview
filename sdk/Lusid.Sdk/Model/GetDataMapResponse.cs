1bdfc3da54b93d1b1a495787fbebe5f11e9750d0
Concourse <concourse@finbourne.com>
2021-07-03 11:27:33 +0000

    Update SDK 0.11.3229

--- sdk/Lusid.Sdk/Model/GetDataMapResponse.cs
+++ sdk/Lusid.Sdk/Model/GetDataMapResponse.cs
@@ -34,7 +34,7 @@ namespace Lusid.Sdk.Model
         /// <param name="href">The specific Uniform Resource Identifier (URI) for this resource at the requested effective and asAt datetime..</param>
         /// <param name="values">TODO.</param>
         /// <param name="failed">TODO.</param>
-        /// <param name="links">Collection of links..</param>
+        /// <param name="links">links.</param>
         public GetDataMapResponse(string href = default(string), Dictionary<string, DataMapping> values = default(Dictionary<string, DataMapping>), Dictionary<string, ErrorDetail> failed = default(Dictionary<string, ErrorDetail>), List<Link> links = default(List<Link>))
         {
             this.Href = href;
@@ -69,9 +69,8 @@ namespace Lusid.Sdk.Model
         public Dictionary<string, ErrorDetail> Failed { get; set; }
 
         /// <summary>
-        /// Collection of links.
+        /// Gets or Sets Links
         /// </summary>
-        /// <value>Collection of links.</value>
         [DataMember(Name="links", EmitDefaultValue=true)]
         public List<Link> Links { get; set; }
 
--- sdk/Lusid.Sdk/Model/UpsertCorporateActionsResponse.cs
+++ sdk/Lusid.Sdk/Model/UpsertCorporateActionsResponse.cs
@@ -34,7 +34,7 @@ namespace Lusid.Sdk.Model
         /// <param name="href">href.</param>
         /// <param name="values">values.</param>
         /// <param name="failed">failed.</param>
-        /// <param name="links">Collection of links..</param>
+        /// <param name="links">links.</param>
         public UpsertCorporateActionsResponse(string href = default(string), Dictionary<string, CorporateAction> values = default(Dictionary<string, CorporateAction>), Dictionary<string, ErrorDetail> failed = default(Dictionary<string, ErrorDetail>), List<Link> links = default(List<Link>))
         {
             this.Href = href;
@@ -66,9 +66,8 @@ namespace Lusid.Sdk.Model
         public Dictionary<string, ErrorDetail> Failed { get; set; }
 
         /// <summary>
-        /// Collection of links.
+        /// Gets or Sets Links
         /// </summary>
-        /// <value>Collection of links.</value>
         [DataMember(Name="links", EmitDefaultValue=true)]
         public List<Link> Links { get; set; }
 
