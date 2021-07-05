c77458d558b1beacab423065fb44bdb92d7ca0e3
Concourse <concourse@finbourne.com>
2021-07-05 21:19:25 +0000

    Update SDK 0.11.3235

--- sdk/Lusid.Sdk/Model/Package.cs
+++ sdk/Lusid.Sdk/Model/Package.cs
@@ -41,7 +41,7 @@ namespace Lusid.Sdk.Model
         /// <param name="orderInstructionIds">A related order instruction id. (required).</param>
         /// <param name="properties">Client-defined properties associated with this execution..</param>
         /// <param name="version">version.</param>
-        /// <param name="links">Collection of links..</param>
+        /// <param name="links">links.</param>
         public Package(ResourceId id = default(ResourceId), List<ResourceId> orderIds = default(List<ResourceId>), List<ResourceId> orderInstructionIds = default(List<ResourceId>), Dictionary<string, PerpetualProperty> properties = default(Dictionary<string, PerpetualProperty>), Version version = default(Version), List<Link> links = default(List<Link>))
         {
             // to ensure "id" is required (not null)
@@ -115,9 +115,8 @@ namespace Lusid.Sdk.Model
         public Version Version { get; set; }
 
         /// <summary>
-        /// Collection of links.
+        /// Gets or Sets Links
         /// </summary>
-        /// <value>Collection of links.</value>
         [DataMember(Name="links", EmitDefaultValue=true)]
         public List<Link> Links { get; set; }
 
