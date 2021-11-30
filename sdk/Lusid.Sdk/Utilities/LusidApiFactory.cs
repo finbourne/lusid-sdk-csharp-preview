123c2b95b1fbf5ba3d90db462f2024179f18fb76
slacki123 <41322879+slacki123@users.noreply.github.com>
2021-11-30 14:05:02 +0000

    Revert "Feature/seng 54.fix null responses (#230)" (#232)
    This reverts commit 2047325bfb508218aced69336c8909672e547c78.
    
    Second PR with this revert as the first one got overridden by concourse's rebase
    
    Co-authored-by: Sebastian Lacki <sebastian.lacki@finbourne.com>

--- sdk/Lusid.Sdk/Utilities/LusidApiFactory.cs
+++ sdk/Lusid.Sdk/Utilities/LusidApiFactory.cs
@@ -75,9 +75,6 @@ namespace Lusid.Sdk.Utilities
                 {
                     throw new Exception($"Unable to create type {api}");
                 }
-                
-                // Replace the default implementation of the ExceptionFactory with a custom one defined by FINBOURNE
-                impl.ExceptionFactory = LusidExceptionHandler.CustomExceptionFactory;
 
                 var @interface = api.GetInterfaces()
                     .First(i => typeof(IApiAccessor).IsAssignableFrom(i));
