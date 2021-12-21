3b453bb1af935e9738cf3c83495538a5d727fb2e
DavidWassellFinbourne <david.wassell@finbourne.com>
2021-12-21 10:01:10 +0000

    SENG-152 Updated Equity Example tests

--- sdk/Lusid.Sdk.Tests/tutorials/Instruments/EquityExample.cs
+++ sdk/Lusid.Sdk.Tests/tutorials/Instruments/EquityExample.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using Lusid.Sdk.Api;
 using Lusid.Sdk.Model;
 using Lusid.Sdk.Tests.Utilities;
-using LusidFeatures;
 using NUnit.Framework;
 
 namespace Lusid.Sdk.Tests.Tutorials.Instruments
@@ -12,25 +11,13 @@ namespace Lusid.Sdk.Tests.Tutorials.Instruments
     [TestFixture]
     public class EquityExample: DemoInstrumentBase
     {
-
-        /*public InstrumentDefinition CreateEquityDefinition(string name, string clientInternal, string domCcy, decimal dividendYield)
-        {
-            SimpleInstrument equity = new SimpleInstrument(instrumentType: LusidInstrument.InstrumentTypeEnum.SimpleInstrument, domCcy: domCcy, assetClass: SimpleInstrument.AssetClassEnum.Equities, simpleInstrumentType: "Equity");
-
-            Property properties = new Property(key: "Instrument/ibor/dividendYield", new PropertyValue(metricValue: new MetricValue(value: dividendYield)));
-
-            InstrumentDefinition equityDefinition = new InstrumentDefinition(name: name, identifiers: new Dictionary<string, InstrumentIdValue>{{"ClientInternal", new InstrumentIdValue(clientInternal)}}, definition: equity, properties: new List<Property>{properties});
-
-            return equityDefinition;
-
-        }*/
-        
         [Test]
         public void EquityCreationAndUpsertionExample()
         {
+            // DEFINE scope
             string scope = "ibor";
             
-            // CREATE an Equity (that can then be upserted into LUSID)
+            // CREATE a Simple Instrument Equity (that can then be upserted into LUSID)
             var equity = (SimpleInstrument) InstrumentExamples.CreateExampleEquity();
             
             // ASSERT that it was created
@@ -60,55 +47,39 @@ namespace Lusid.Sdk.Tests.Tutorials.Instruments
                 Console.WriteLine(e);
             }
             
-            // DEFINE properties
+            // DEFINE properties for Equity example
             decimal dividendYield = (decimal)0.88;
-            Property properties = new Property(key: "Instrument/ibor/dividendYield", value: new PropertyValue(metricValue: new MetricValue(value: dividendYield)));
+            Property properties = new Property(key: $"Instrument/{scope}/dividendYield", value: new PropertyValue(metricValue: new MetricValue(value: dividendYield)));
             
             // DEFINE equity instrument definition with property
             string name = "Microsoft";
-            string identifier = "MSFT";
-            var equityDefinition = new InstrumentDefinition(name: name, identifiers: new Dictionary<string, InstrumentIdValue>{{"ClientInternal", new InstrumentIdValue(identifier)}}, definition: equity, properties: new List<Property>{properties});
+            var uniqueId = equity.InstrumentType+Guid.NewGuid().ToString(); 
+            var equityDefinition = new InstrumentDefinition(name: name, identifiers: new Dictionary<string, InstrumentIdValue>{{"ClientInternal", new InstrumentIdValue(uniqueId)}}, definition: equity, properties: new List<Property>{properties});
             
-            // CAN NOW UPSERT TO LUSID
-            Dictionary<string, InstrumentDefinition> upsertRequest = new Dictionary<string, InstrumentDefinition> {{identifier, equityDefinition}};
+            // UPSERT TO LUSID
+            Dictionary<string, InstrumentDefinition> upsertRequest = new Dictionary<string, InstrumentDefinition> {{uniqueId, equityDefinition}};
             var upsertResponse = _instrumentsApi.UpsertInstruments(upsertRequest);
             ValidateUpsertInstrumentResponse(upsertResponse);
             
-            // CAN NOW QUERY FROM LUSID
-            var getResponse = _instrumentsApi.GetInstruments(identifierType: "ClientInternal", requestBody: new List<String> {identifier}, propertyKeys:new List<String> { "Instrument/ibor/dividendYield"});
-            ValidateInstrumentResponse(getResponse, identifier);
+            // QUERY FROM LUSID
+            var getResponse = _instrumentsApi.GetInstruments(identifierType: "ClientInternal", requestBody: new List<String> {uniqueId}, propertyKeys:new List<String> {$"Instrument/{scope}/dividendYield"});
+            ValidateInstrumentResponse(getResponse, uniqueId);
             
-
-            var retrieved = getResponse.Values.First().Value.InstrumentDefinition;
-            Assert.That(retrieved.InstrumentType == LusidInstrument.InstrumentTypeEnum.SimpleInstrument);
-            var roundTripEquity = retrieved as SimpleInstrument;
+            // ASSERT that equity instrument and properties were retrieved from LUSID
+            var retrievedDefinition = getResponse.Values.First().Value.InstrumentDefinition;
+            var retrievedProperties = getResponse.Values.First().Value.Properties;
+            Assert.That(retrievedDefinition.InstrumentType == LusidInstrument.InstrumentTypeEnum.SimpleInstrument);
+            Assert.That(retrievedProperties.First().Key, Is.EqualTo($"Instrument/{scope}/dividendYield"));
+            Assert.That(retrievedProperties.First().Value, Is.EqualTo(new PropertyValue(metricValue: new MetricValue(value: dividendYield))));
+            var roundTripEquity = retrievedDefinition as SimpleInstrument;
             Assert.That(roundTripEquity, Is.Not.Null);
             Assert.That(roundTripEquity.AssetClass, Is.EqualTo(SimpleInstrument.AssetClassEnum.Equities));
-            // Assert.That(roundTripEquity., Is.EqualTo(SimpleInstrument.AssetClassEnum.Equities));
-
-
-
-            // CAN NOW QUERY FROM LUSID
-            // var getResponse = _instrumentsApi.GetInstruments("ClientInternal", new List<string> { uniqueId });
-            // ValidateInstrumentResponse(getResponse, uniqueId);
-
-            // var retrieved = getResponse.Values.First().Value.InstrumentDefinition;
-            // Assert.That(retrieved.InstrumentType == LusidInstrument.InstrumentTypeEnum.SimpleInstrument);
-            // var roundTripEquity = retrieved as SimpleInstrument;
-            // Assert.That(roundTripEquity, Is.Not.Null);
-
-            /*string name = "Microsoft";
-            string identifier = "MSFT";
-            string domCcy = "USD";
-            decimal dividendYield = (Decimal)0.88;
-
-            var equityExampleDefinition = CreateEquityDefinition(name: name, clientInternal: identifier, domCcy: domCcy, dividendYield: dividendYield);
+            Assert.That(roundTripEquity.DomCcy, Is.EqualTo("USD"));
+            Assert.That(roundTripEquity.InstrumentType, Is.EqualTo(LusidInstrument.InstrumentTypeEnum.SimpleInstrument));
+            Assert.That(roundTripEquity.SimpleInstrumentType, Is.EqualTo("Equity"));
             
-            Dictionary<string, InstrumentDefinition> upsertRequest = new Dictionary<string, InstrumentDefinition> {{identifier, equityExampleDefinition}};
-            
-            var upsertResponse = _instrumentsApi.UpsertInstruments(upsertRequest);
-
-            Assert.That(upsertResponse, Is.Not.Null);*/
+            // DELETE Instrument 
+            _instrumentsApi.DeleteInstrument("ClientInternal", identifier: uniqueId); 
         }
 
         internal override void CreateAndUpsertMarketDataToLusid(string scope, ModelSelection.ModelEnum model, LusidInstrument instrument)
