eff9fd2068bbf25cad7ba2f2c1e6535850af7372
jaggerglyn <97467402+jaggerglyn@users.noreply.github.com>
2022-01-11 18:22:54 +0000

    CTECH-672: Addition of Test_Transaction_Property (feature F13-4) (#252)

--- sdk/Lusid.Sdk.Tests/tutorials/Ibor/Transactions.cs
+++ sdk/Lusid.Sdk.Tests/tutorials/Ibor/Transactions.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using Lusid.Sdk.Api;
+using Lusid.Sdk.Client;
 using Lusid.Sdk.Model;
 using Lusid.Sdk.Tests.Utilities;
 using Lusid.Sdk.Utilities;
@@ -45,29 +46,8 @@ namespace Lusid.Sdk.Tests.Tutorials.Ibor
         [Test]
         public void Load_Listed_Instrument_Transaction()
         {
-            var effectiveDate = new DateTimeOffset(2018, 1, 1, 0, 0, 0, TimeSpan.Zero);
-            
-            //    create the portfolio
-            //var portfolioCode = _testDataUtilities.CreateTransactionPortfolio(TestDataUtilities.TutorialScope);
             //    create the transaction request
-            var transaction = new TransactionRequest(
-                
-                //    unique transaction id
-                transactionId: Guid.NewGuid().ToString(),
-                
-                //    instruments must already exist in LUSID and have a valid LUSID instrument id
-                instrumentIdentifiers: new Dictionary<string, string>
-                {
-                    [TestDataUtilities.LusidInstrumentIdentifier] = _instrumentIds[0]
-                },
-                
-                type: "Buy",
-                totalConsideration: new CurrencyAndAmount(1230, "GBP"),
-                transactionDate: effectiveDate,
-                settlementDate: effectiveDate,
-                units: 100,
-                transactionPrice: new TransactionPrice(12.3M),
-                source: "Custodian");
+            var transaction = BuildTransactionRequest();
             
             //    add the transaction
             _transactionPortfoliosApi.UpsertTransactions(TestDataUtilities.TutorialScope, _portfolioCode, new List<TransactionRequest> {transaction});
@@ -85,15 +65,12 @@ namespace Lusid.Sdk.Tests.Tutorials.Ibor
         {
             var effectiveDate = new DateTimeOffset(2018, 1, 1, 0, 0, 0, TimeSpan.Zero);
             
-            //    create the portfolio
-            //var portfolioCode = _testDataUtilities.CreateTransactionPortfolio(TestDataUtilities.TutorialScope);
             //    create the transaction request
             var transaction = new TransactionRequest(
                 
                 //    unique transaction id
                 transactionId: Guid.NewGuid().ToString(),
                 
-                //    instruments must already exist in LUSID and have a valid LUSID instrument id
                 instrumentIdentifiers: new Dictionary<string, string>
                 {
                     [TestDataUtilities.LusidCashIdentifier] = "GBP"
@@ -243,6 +220,97 @@ namespace Lusid.Sdk.Tests.Tutorials.Ibor
             var noTransactions = _transactionPortfoliosApi.GetTransactions(TestDataUtilities.TutorialScope, _portfolioCode);
 
             Assert.That(noTransactions.Values, Is.Empty);
+        }
+
+        [LusidFeature("F13-4")]
+        [Test]
+        public void Add_Transactions_To_Portfolio_With_Property()
+        {
+            // create a transaction request
+            var transactionRequest = BuildTransactionRequest();
+
+            // add the transaction property we are testing
+            string code = "ExecutingTrader";
+            string executingTraderKey = $"Transaction/{TestDataUtilities.TutorialScope}/{code}";
+            string executingTraderValue = "Glyn Jagger";
+            EnsurePropertyDefinition("Transaction", code);
+            transactionRequest.Properties = new Dictionary<string, PerpetualProperty>
+            {
+                {
+                    executingTraderKey,
+                    new PerpetualProperty(executingTraderKey, new PropertyValue(labelValue: executingTraderValue))
+                }
+            };
+
+            // add the transaction
+            UpsertPortfolioTransactionsResponse upsertResp = _transactionPortfoliosApi.UpsertTransactions(TestDataUtilities.TutorialScope, _portfolioCode, new List<TransactionRequest> { transactionRequest });
+
+            // get the transaction and verify the reponse
+            var transactions = _transactionPortfoliosApi.GetTransactions(TestDataUtilities.TutorialScope, _portfolioCode);
+
+            Assert.That(transactions.Values, Has.Count.EqualTo(1));
+            Assert.That(transactions.Values[0].TransactionId, Is.EqualTo(transactionRequest.TransactionId));
+
+            // assert that the custom properties was upserted with the transaction and is returned
+            Assert.IsTrue(transactions.Values[0].Properties.ContainsKey(executingTraderKey));
+            Assert.That(transactions.Values[0].Properties[executingTraderKey].Value.LabelValue == executingTraderValue);
+        }
+
+        private TransactionRequest BuildTransactionRequest()
+        {
+            var effectiveDate = new DateTimeOffset(2018, 1, 1, 0, 0, 0, TimeSpan.Zero);
+            return new TransactionRequest(
+                // unique transaction id
+                transactionId: Guid.NewGuid().ToString(),
+
+                instrumentIdentifiers: new Dictionary<string, string>
+                {
+                    [TestDataUtilities.LusidInstrumentIdentifier] = _instrumentIds[0]
+                },
+
+                type: "Buy",
+                totalConsideration: new CurrencyAndAmount(1230, "GBP"),
+                transactionDate: effectiveDate,
+                settlementDate: effectiveDate,
+                units: 100,
+                transactionPrice: new TransactionPrice(12.3M),
+                source: "Custodian"
+            );
+        }
+
+        private void EnsurePropertyDefinition(string domain, string code)
+        {
+            var propertyApi = _apiFactory.Api<IPropertyDefinitionsApi>();
+
+            if (!Enum.TryParse(domain, false, out CreatePropertyDefinitionRequest.DomainEnum domainEnum))
+                throw new ArgumentException($"Specified domain:{domain} is invalid.", "domain");
+
+            try
+            {
+                propertyApi.GetPropertyDefinition(domain, TestDataUtilities.TutorialScope, code);
+            }
+            catch (ApiException apiEx)
+            {
+                if (apiEx.ErrorCode == 404) {
+                    //    Property definition doesn't exist (returns 404), so create one
+                    //    Details of the property to be created
+                    var propertyDefinition = new CreatePropertyDefinitionRequest(
+                        domain: domainEnum,
+                        scope: TestDataUtilities.TutorialScope,
+                        lifeTime: CreatePropertyDefinitionRequest.LifeTimeEnum.Perpetual,
+                        code: code,
+                        valueRequired: false,
+                        displayName: code,
+                        dataTypeId: new ResourceId("system", "string")
+                    );
+
+                    //    Create the property
+                    propertyApi.CreatePropertyDefinition(propertyDefinition);
+                }
+                else {  
+                    throw apiEx; 
+                }
+            }
         }
     }
 }
