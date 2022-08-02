db91f03020829716ea1c8575a75658ffbb024994
Matthew Fox <matthew.fox@finbourne.com>
2022-08-02 11:14:56 +0100

    CTECH-1502: Add tests and implementation for setting timeout via the ApiFactory.

--- sdk/Lusid.Sdk.Tests/LusidApiFactoryTests.cs
+++ sdk/Lusid.Sdk.Tests/LusidApiFactoryTests.cs
@@ -523,5 +523,32 @@ namespace Lusid.Sdk.Tests
             // assert
             Assert.IsEmpty(api.Configuration.DefaultHeaders);
         }
+
+        [Test]
+        public void ApiFactoryWithTimeout() 
+        {
+
+            int timeout = 200;
+            int defaultTimeout = 100000;
+
+            var factory = LusidApiFactoryBuilder.Build("secrets.json", timeout);
+
+            var api = factory.Api<ScopesApi>();
+            DateTime start = DateTime.Now;
+
+            Assert.That(() => api.ListScopes(null), Throws.InstanceOf<ApiException>().With.Message.EqualTo("Internal SDK error occurred when calling ListScopes: The operation has timed out."));
+
+            try
+            {
+                start = DateTime.Now;
+                var scopes = api.ListScopes(null);
+            } catch 
+            {
+                DateTime finish = DateTime.Now;
+                TimeSpan elapsed = finish - start;
+
+                Assert.That(elapsed.TotalMilliseconds, Is.LessThanOrEqualTo((long)defaultTimeout));
+            }
+        }
     }
 }
