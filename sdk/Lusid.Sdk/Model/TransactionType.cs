f015c3250e1f23ba875a771992f67c40590bb854
Concourse <concourse@finbourne.com>
2023-04-07 10:01:44 +0000

    Update SDK 1.0.63 [rebase]

--- sdk/Lusid.Sdk/Model/TransactionType.cs
+++ sdk/Lusid.Sdk/Model/TransactionType.cs
@@ -43,7 +43,7 @@ namespace Lusid.Sdk.Model
         /// <param name="aliases">List of transaction types that map to this specific transaction configuration (required).</param>
         /// <param name="movements">Movement data for the transaction type (required).</param>
         /// <param name="properties">Properties attached to the transaction type.</param>
-        /// <param name="links">Collection of links..</param>
+        /// <param name="links">links.</param>
         public TransactionType(List<TransactionTypeAlias> aliases = default(List<TransactionTypeAlias>), List<TransactionTypeMovement> movements = default(List<TransactionTypeMovement>), Dictionary<string, PerpetualProperty> properties = default(Dictionary<string, PerpetualProperty>), List<Link> links = default(List<Link>))
         {
             // to ensure "aliases" is required (not null)
@@ -76,9 +76,8 @@ namespace Lusid.Sdk.Model
         public Dictionary<string, PerpetualProperty> Properties { get; set; }
 
         /// <summary>
-        /// Collection of links.
+        /// Gets or Sets Links
         /// </summary>
-        /// <value>Collection of links.</value>
         [DataMember(Name = "links", EmitDefaultValue = true)]
         public List<Link> Links { get; set; }
 
