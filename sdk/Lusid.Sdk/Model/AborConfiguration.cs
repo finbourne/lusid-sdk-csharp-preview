f0550ee43a52a2e745cd712ccfa4b2c52a0314e9
Concourse <concourse@finbourne.com>
2023-06-24 09:32:08 +0000

    Update SDK 1.0.284 [rebase]

--- sdk/Lusid.Sdk/Model/AborConfiguration.cs
+++ sdk/Lusid.Sdk/Model/AborConfiguration.cs
@@ -48,7 +48,7 @@ namespace Lusid.Sdk.Model
         /// <param name="chartOfAccountsId">chartOfAccountsId.</param>
         /// <param name="properties">Properties to add to the AborConfiguration..</param>
         /// <param name="version">version.</param>
-        /// <param name="links">Collection of links..</param>
+        /// <param name="links">links.</param>
         public AborConfiguration(string href = default(string), ResourceId id = default(ResourceId), string description = default(string), string name = default(string), ResourceId recipeId = default(ResourceId), ResourceId chartOfAccountsId = default(ResourceId), Dictionary<string, Property> properties = default(Dictionary<string, Property>), Version version = default(Version), List<Link> links = default(List<Link>))
         {
             // to ensure "id" is required (not null)
@@ -116,9 +116,8 @@ namespace Lusid.Sdk.Model
         public Version Version { get; set; }
 
         /// <summary>
-        /// Collection of links.
+        /// Gets or Sets Links
         /// </summary>
-        /// <value>Collection of links.</value>
         [DataMember(Name = "links", EmitDefaultValue = true)]
         public List<Link> Links { get; set; }
 
--- sdk/Lusid.Sdk/Model/TaxRuleSet.cs
+++ sdk/Lusid.Sdk/Model/TaxRuleSet.cs
@@ -46,7 +46,7 @@ namespace Lusid.Sdk.Model
         /// <param name="outputPropertyKey">The property key that this rule set will write to (required).</param>
         /// <param name="rules">The rules of this rule set, which stipulate what rate to apply (i.e. write to the OutputPropertyKey) under certain conditions (required).</param>
         /// <param name="version">version.</param>
-        /// <param name="links">Collection of links..</param>
+        /// <param name="links">links.</param>
         public TaxRuleSet(ResourceId id = default(ResourceId), string displayName = default(string), string description = default(string), string outputPropertyKey = default(string), List<TaxRule> rules = default(List<TaxRule>), Version version = default(Version), List<Link> links = default(List<Link>))
         {
             // to ensure "id" is required (not null)
@@ -104,9 +104,8 @@ namespace Lusid.Sdk.Model
         public Version Version { get; set; }
 
         /// <summary>
-        /// Collection of links.
+        /// Gets or Sets Links
         /// </summary>
-        /// <value>Collection of links.</value>
         [DataMember(Name = "links", EmitDefaultValue = true)]
         public List<Link> Links { get; set; }
 
