3bf2c56f6eb32f487e535ff69057e35582317bf0
Concourse <concourse@finbourne.com>
2024-11-01 20:02:26 +0000

    Updates for v1.1.436

--- sdk/Lusid.Sdk/Model/ComplianceTemplateVariation.cs
+++ sdk/Lusid.Sdk/Model/ComplianceTemplateVariation.cs
@@ -46,7 +46,8 @@ namespace Lusid.Sdk.Model
         /// <param name="properties">Properties associated with the Compliance Template Variation (required).</param>
         /// <param name="acceptedAddressKeys">acceptedAddressKeys (required).</param>
         /// <param name="steps">The steps expressed in this template, with their required parameters (required).</param>
-        public ComplianceTemplateVariation(string label = default(string), string description = default(string), List<ComplianceTemplateParameter> requiredParameters = default(List<ComplianceTemplateParameter>), Dictionary<string, PerpetualProperty> properties = default(Dictionary<string, PerpetualProperty>), ResourceId acceptedAddressKeys = default(ResourceId), List<ComplianceStep> steps = default(List<ComplianceStep>))
+        /// <param name="referencedGroupLabel">The label of a given referenced group in a Compliance Rule Template Variation.</param>
+        public ComplianceTemplateVariation(string label = default(string), string description = default(string), List<ComplianceTemplateParameter> requiredParameters = default(List<ComplianceTemplateParameter>), Dictionary<string, PerpetualProperty> properties = default(Dictionary<string, PerpetualProperty>), ResourceId acceptedAddressKeys = default(ResourceId), List<ComplianceStep> steps = default(List<ComplianceStep>), string referencedGroupLabel = default(string))
         {
             // to ensure "label" is required (not null)
             this.Label = label ?? throw new ArgumentNullException("label is a required property for ComplianceTemplateVariation and cannot be null");
@@ -60,6 +61,7 @@ namespace Lusid.Sdk.Model
             this.AcceptedAddressKeys = acceptedAddressKeys ?? throw new ArgumentNullException("acceptedAddressKeys is a required property for ComplianceTemplateVariation and cannot be null");
             // to ensure "steps" is required (not null)
             this.Steps = steps ?? throw new ArgumentNullException("steps is a required property for ComplianceTemplateVariation and cannot be null");
+            this.ReferencedGroupLabel = referencedGroupLabel;
         }
 
         /// <summary>
@@ -104,6 +106,13 @@ namespace Lusid.Sdk.Model
         public List<ComplianceStep> Steps { get; set; }
 
         /// <summary>
+        /// The label of a given referenced group in a Compliance Rule Template Variation
+        /// </summary>
+        /// <value>The label of a given referenced group in a Compliance Rule Template Variation</value>
+        [DataMember(Name = "referencedGroupLabel", EmitDefaultValue = true)]
+        public string ReferencedGroupLabel { get; set; }
+
+        /// <summary>
         /// Returns the string presentation of the object
         /// </summary>
         /// <returns>String presentation of the object</returns>
@@ -117,6 +126,7 @@ namespace Lusid.Sdk.Model
             sb.Append("  Properties: ").Append(Properties).Append("\n");
             sb.Append("  AcceptedAddressKeys: ").Append(AcceptedAddressKeys).Append("\n");
             sb.Append("  Steps: ").Append(Steps).Append("\n");
+            sb.Append("  ReferencedGroupLabel: ").Append(ReferencedGroupLabel).Append("\n");
             sb.Append("}\n");
             return sb.ToString();
         }
@@ -183,6 +193,11 @@ namespace Lusid.Sdk.Model
                     this.Steps != null &&
                     input.Steps != null &&
                     this.Steps.SequenceEqual(input.Steps)
+                ) && 
+                (
+                    this.ReferencedGroupLabel == input.ReferencedGroupLabel ||
+                    (this.ReferencedGroupLabel != null &&
+                    this.ReferencedGroupLabel.Equals(input.ReferencedGroupLabel))
                 );
         }
 
@@ -207,6 +222,8 @@ namespace Lusid.Sdk.Model
                     hashCode = hashCode * 59 + this.AcceptedAddressKeys.GetHashCode();
                 if (this.Steps != null)
                     hashCode = hashCode * 59 + this.Steps.GetHashCode();
+                if (this.ReferencedGroupLabel != null)
+                    hashCode = hashCode * 59 + this.ReferencedGroupLabel.GetHashCode();
                 return hashCode;
             }
         }
